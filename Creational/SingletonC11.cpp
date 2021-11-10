0633d96cfd46c7dbc84168b314ffc82304daadc3
physicsX <ulasdikme@gmail.com>
2021-11-10 23:28:21 +0100

    Singleton example for c++11 style with unique ptr

---
+++ Creational/SingletonC11.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <memory>
+
+
+/*
+	Singleton Design Pattern Example with unique_ptr<>
+	No leak possible
+	g++ Singleton.cpp -std=c++14 -o singleton
+	thread safe
+*/
+
+class Singleton
+{
+	private:
+		static std::unique_ptr<Singleton> instance;
+		struct _cons {explicit _cons() = default; };
+
+	public:
+		int X;
+		Singleton(_cons){std::cout<<"const is called"<<std::endl;}
+		static std::unique_ptr<Singleton> instanceFactory()
+		{
+			return std::make_unique<Singleton>(_cons{});
+		}
+		
+	static std::unique_ptr<Singleton>& getInstance();
+
+	~Singleton(){std::cout<<"Dest is called"<<std::endl;}
+	void setX(int x)
+	{
+		X = x;
+	}
+	int getX()
+	{
+		return X;
+	}
+
+};
+
+std::unique_ptr<Singleton>& Singleton::getInstance()
+{
+	static std::unique_ptr<Singleton> instance { Singleton::instanceFactory() };
+	return instance;
+}
+
+int main()
+{
+
+	//std::unique_ptr<Singleton> s = std::make_unique<Singleton>; //Error	
+
+	std::unique_ptr<Singleton> &p = Singleton::getInstance();
+	p->setX(1);
+	std::cout<<"X: "<<p->getX()<<std::endl;
+
+	std::unique_ptr<Singleton> &p2 = Singleton::getInstance();
+	std::cout<<"X: "<<p2->getX()<<std::endl;
+
+	p2->setX(2);
+	std::cout<<"X: "<<p->getX()<<std::endl;
+
+
+
+	return 0;
+}
