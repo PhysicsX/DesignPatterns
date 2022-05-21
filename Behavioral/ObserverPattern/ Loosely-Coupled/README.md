f343ee70aa4430944ca6d07f9f5c53462931221f
physicsX <ulasdikme@gmail.com>
2022-05-21 20:15:14 +0200

    Add loosely coupled observer example

---
+++ Behavioral/ObserverPattern/ Loosely-Coupled/README.md
@@ -0,0 +1,2 @@
+Loosely-coupled
+![Class Diagram](https://www.plantuml.com/plantuml/proxy?src=https://raw.githubusercontent.com/PhysicsX/DesignPatterns/master/Behavioral/ObserverPattern/Loosely-Coupled/UML/Instance.puml)
---
+++ Behavioral/ObserverPattern/ Loosely-Coupled/observer.cpp
@@ -0,0 +1,107 @@
+// Example program
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+class observerIf
+{
+    public:
+    virtual void update() = 0;
+    virtual ~observerIf(){};
+};
+
+class subjectIf
+{
+    public:
+
+    virtual void remove(observerIf* obs) = 0;
+
+    virtual void addObs(observerIf* obs) = 0;
+
+    virtual void notify() = 0;
+
+    protected:
+    std::vector<observerIf*> mListObs;
+};
+
+class subject : public subjectIf
+{
+    public:
+    subject(){}
+    void addObs(observerIf* obs)
+    {
+        mListObs.push_back(obs);
+    }
+    
+    void notify()
+    {
+        for(auto s : mListObs)
+            s->update();
+    }
+
+    void remove(observerIf* obs)
+    {   
+        std::vector<observerIf*>::iterator position = std::find(mListObs.begin(), mListObs.end(), obs);
+        if (position != mListObs.end())
+        {
+            mListObs.erase(position);
+        }
+    }
+};
+
+class concObserver : public observerIf
+{
+    public:
+    concObserver(std::function<void(observerIf*)>& remove,
+                      std::function<void(observerIf*)>& add)
+    :mCallbackRemove(remove),
+    mCallbackAdd(add)
+    {
+        mCallbackAdd(this);
+    }
+
+    void update(){
+        std::cout<<"concObserverCall4 is updated"<<std::endl;
+    }
+
+    ~concObserver()
+    {
+        mCallbackRemove(this);
+    }
+
+    private:
+
+    std::function<void(observerIf*)>& mCallbackRemove;
+    std::function<void(observerIf*)>& mCallbackAdd;
+
+};
+
+int main()
+{
+    subjectIf* sbj = new subject;
+    std::cout<<sbj<<std::endl;
+
+    // callback function to distribute to all observers
+    // to remove them from the list
+    std::function<void(observerIf*)> removeCallBack
+    = std::bind(&subject::remove, dynamic_cast<subject*>(sbj), std::placeholders::_1);
+    
+    std::function<void(observerIf*)> addCallBack
+    = std::bind(&subject::addObs, dynamic_cast<subject*>(sbj), std::placeholders::_1);
+
+
+    std::cout<<"Create concObserver"<<std::endl;
+    observerIf* obsCall4 = 
+    new concObserver(removeCallBack, addCallBack);
+    sbj->notify();
+    
+    std::cout<<"delete concObserver"<<std::endl;
+    delete obsCall4;
+    sbj->notify();
+
+
+    delete sbj;
+    std::cout<<"end"<<std::endl;
+}
